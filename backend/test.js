38560d1d436bb2ab72cafd28c4e97afee6cfab96
Student User <student@workstation.lab.example.com>
2023-06-21 07:29:02 -0400

    Using a declarative pipeline

--- backend/test.js
+++ backend/test.js
@@ -2,4 +2,4 @@ const assert = require("assert").strict;
 const greet = require("./greet");
 
 assert.strictEqual(greet(), "Hello guest");
-assert.strictEqual(greet("Guy"), "Hello Guy");
+assert.strictEqual(greet("Guys"), "Hello Guy");
